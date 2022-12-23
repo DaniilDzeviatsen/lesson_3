3dadfd48b111181e661715c982217541c596ec26
DaniilDzeviatsen <deviat.daan@gmail.com>
2022-12-23 21:53:54 +0300

    simple

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
         System.out.println("Введите год вашего рождения");
         Scanner m = new Scanner (System.in);
         int year=m.nextInt();
+        int n=2022-year;
+        if (n<18 || n>100){
+            System.out.println("Forbid to get drunk");
+        }
+
         System.out.println("Ваш возраст равен "+(2022-year)+" годам");
+
     }
 }
