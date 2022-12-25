a4c18b7532a591ab58414039fff25c606fd0158b
DaniilDzeviatsen <118615565+DaniilDzeviatsen@users.noreply.github.com>
2022-12-25 09:27:46 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arr=new int[13];
+        for (int i=0; i<13; i++){
+            arr[i]=i;}
+        for (int i=2;i<13;i++){
+            arr [i]=arr[i-1]+arr [i-2];
+            System.out.println(arr[i-2]);
+        }
+
+
     }
 }
